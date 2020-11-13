1c1b31005497bea8c75e11ddaa5b346f6c31cbf5
David L. Inman <davidlevere@gmail.com>
2020-11-12 23:11:52 -0500

    todo

--- src/parsing/parse_type.rs
+++ src/parsing/parse_type.rs
@@ -2,8 +2,23 @@
 
 use parse_input::{PSym, Input, ParseError};
 
-use super::ast::{Type};
+use super::ast::Type;
 
 pub fn parse( input : Input ) -> Result<Type, ParseError> {
     Ok(Type::Void)
 }
+
+/*
+
+    void
+    fun(type array) -> type
+    [type]
+    'name
+    sym<type>
+    sym::sym<type>
+    Simple
+    sym::Simple
+    { sym : type, ... | rest }
+    Dict<type, type>
+
+*/
